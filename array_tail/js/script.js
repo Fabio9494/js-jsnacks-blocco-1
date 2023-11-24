3c0c152d4aa59d893315187f75ecb38f05399b72
Fabio9494 <fabio.casoria94@gmail.com>
2023-11-24 14:52:20 +0100

    add bonus

--- array_tail/js/script.js
+++ array_tail/js/script.js
@@ -4,5 +4,21 @@ for(let i=0;i<length_array;i++){
     n = Math.floor(Math.random() * 100);
     array.push(n);
 }
+console.log("array");
 console.log(array);
-console.log(array.slice(length_array-5,length_array));
+if(length_array>=5){
+    console.log(array.slice(length_array-5,length_array));
+}
+else {
+    console.log("INSERISCI UN NUMERO DI ELEMTI MAGGIORE O UGUALE A 5")
+}
+
+//bonus
+let = coda = parseInt(prompt("numero elementi da stampare"));
+console.log("elenti da stampare: ultimi "+coda);
+if(coda<=length_array){
+    console.log(array.slice(length_array-coda,length_array));
+}
+else{
+    console.log("INSERISCI UN NUMERO DI ELEMTI MINORE O UGUALE ALLA LUNGHEZZA DELL'ARRAY")
+}
